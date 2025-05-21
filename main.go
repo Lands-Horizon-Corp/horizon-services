9cd249d763a0e2fb59302f6369c5793e0f1949e5
zalven-official <zalvendayao888@gmail.com>
2025-05-21 23:58:26 +0800

    adding lifetime configs

--- main.go
+++ main.go
@@ -8,8 +8,31 @@ import (
 )
 
 func main() {
-
-	horizon := services.NewHorizonService()
+	horizon := services.NewHorizonService(services.HorizonServiceConfig{
+		SQLConfig: &services.SQLServiceConfig{
+			DSN:         "",
+			MaxIdleConn: 10,
+			MaxOpenConn: 100,
+			MaxLifetime: 60,
+		},
+		StorageConfig: &services.StorageServiceConfig{
+			AccessKey:   "",
+			SecretKey:   "",
+			Prefix:      "",
+			Bucket:      "",
+			MaxFilezize: 1024 * 1024 * 10, // 10 MB
+		},
+		CacheConfig: &services.CacheServiceConfig{
+			Host:     "",
+			Password: "",
+			Username: "",
+			Port:     17356,
+		},
+		BrokerConfig: &services.BrokerServiceConfig{
+			Host: "",
+			Port: 4222,
+		},
+	})
 
 	if err := horizon.Run(context.Background()); err != nil {
 		fmt.Println("Error:", err)
--- services/service.go
+++ services/service.go
@@ -14,61 +14,93 @@ type HorizonService struct {
 	Cron     horizon.SchedulerService
 }
 
-func NewHorizonService(
-	sqlConfig SQLServiceConfig,
-	storageConfig StorageServiceConfig,
-	cacheConfig CacheServiceConfig,
-	brokerConfig BrokerServiceConfig,
-) *HorizonService {
-	return &HorizonService{
-		Database: horizon.NewGormDatabase(
-			sqlConfig.DSN,
-			sqlConfig.MaxIdleConn,
-			sqlConfig.MaxOpenConn,
-			sqlConfig.MaxLifetime,
-		),
-		Storage: horizon.NewHorizonStorageService(
-			storageConfig.AccessKey,
-			storageConfig.SecretKey,
-			storageConfig.Prefix,
-			storageConfig.Bucket,
-			storageConfig.MaxFilezize,
-		),
-		Cache: horizon.NewHorizonCache(
-			cacheConfig.Host,
-			cacheConfig.Password,
-			cacheConfig.Username,
-			cacheConfig.Port,
-		),
-		Broker: horizon.NewHorizonMessageBroker(
-			brokerConfig.Host,
-			brokerConfig.Port,
-		),
-		Cron: horizon.NewHorizonSchedule(),
+type HorizonServiceConfig struct {
+	SQLConfig     *SQLServiceConfig
+	StorageConfig *StorageServiceConfig
+	CacheConfig   *CacheServiceConfig
+	BrokerConfig  *BrokerServiceConfig
+}
+
+func NewHorizonService(cfg HorizonServiceConfig) *HorizonService {
+	service := &HorizonService{}
+
+	if cfg.SQLConfig != nil {
+		service.Database = horizon.NewGormDatabase(
+			cfg.SQLConfig.DSN,
+			cfg.SQLConfig.MaxIdleConn,
+			cfg.SQLConfig.MaxOpenConn,
+			cfg.SQLConfig.MaxLifetime,
+		)
 	}
+
+	if cfg.StorageConfig != nil {
+		service.Storage = horizon.NewHorizonStorageService(
+			cfg.StorageConfig.AccessKey,
+			cfg.StorageConfig.SecretKey,
+			cfg.StorageConfig.Prefix,
+			cfg.StorageConfig.Bucket,
+			cfg.StorageConfig.MaxFilezize,
+		)
+	}
+
+	if cfg.CacheConfig != nil {
+		service.Cache = horizon.NewHorizonCache(
+			cfg.CacheConfig.Host,
+			cfg.CacheConfig.Password,
+			cfg.CacheConfig.Username,
+			cfg.CacheConfig.Port,
+		)
+	}
+
+	if cfg.BrokerConfig != nil {
+		service.Broker = horizon.NewHorizonMessageBroker(
+			cfg.BrokerConfig.Host,
+			cfg.BrokerConfig.Port,
+		)
+	}
+
+	// Scheduler (Cron) is always initialized if needed
+	service.Cron = horizon.NewHorizonSchedule()
+
+	return service
 }
 
 func (h *HorizonService) Run(ctx context.Context) error {
-	if err := h.Cron.Run(ctx); err != nil {
-		return err
+	if h.Cron != nil {
+		if err := h.Cron.Run(ctx); err != nil {
+			return err
+		}
 	}
-	if err := h.Broker.Run(ctx); err != nil {
-		return err
+
+	if h.Broker != nil {
+		if err := h.Broker.Run(ctx); err != nil {
+			return err
+		}
 	}
-	if err := h.Cache.Run(ctx); err != nil {
-		return err
+
+	if h.Cache != nil {
+		if err := h.Cache.Run(ctx); err != nil {
+			return err
+		}
+		if err := h.Cache.Ping(ctx); err != nil {
+			return err
+		}
 	}
-	if err := h.Cache.Ping(ctx); err != nil {
-		return err
+
+	if h.Storage != nil {
+		if err := h.Storage.Run(ctx); err != nil {
+			return err
+		}
 	}
-	if err := h.Storage.Run(ctx); err != nil {
-		return err
+
+	if h.Database != nil {
+		if err := h.Database.Run(ctx); err != nil {
+			return err
+		}
+		if err := h.Database.Ping(ctx); err != nil {
+			return err
+		}
 	}
-	if err := h.Database.Run(ctx); err != nil {
-		return err
-	}
-	if err := h.Database.Ping(ctx); err != nil {
-		return err
-	}
+
 	return nil
 }
